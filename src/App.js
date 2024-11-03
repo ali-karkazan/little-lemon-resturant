99d03edcbf210e5f293c0fbe5f005a4fc20bffc8
ali-karkazan <ali.karkazan@icloud.com>
2024-11-03 12:40:01 +0400

    Create Menu component with online order function

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Nav from './components/Nav'
 import Main from './components/Main';
+import Menu from './components/Menu'
 
 function App() {
   return (
     <>
       <Nav />
       <Main />
+      <Menu />
     </>
   );
 }
--- src/recipes.js
+++ src/recipes.js
@@ -1,31 +1,30 @@
 const recipes = [
-    { 
-      id: 1,
-      title: "Greek salad",
-      price: 12.99,
-      image: "https://i.ibb.co/68c9bLv/greek-salad.jpg",
-      description:
-        "The famous greek salad of crispy lettuce, peppers, olives and our Chicago style fota cheese, garnished with crunchy garlic and rosemary croutons",
-    },
-    {
-      id: 2,
-      title: "Bruchetta",
-      price: 5.99,
-      image: "https://i.ibb.co/317GqZv/Bruchetta.png",
-      description:
-        "Our Bruschetta is made from grilled bread that has been smeared with garlic and seasoned with salt and olive oil.",
-    },
-    {
-      id: 3,
-      title: "Lemon Dessert",
-      price: 4.78,
-      image: "https://i.ibb.co/sm6MfcH/desert.jpg",
-      description:
-        "This comes straight from grandma's recipe book, every last ingredient has been sourced and is as authentic as can be imagined.",
-    },
-  
-  ];
-  
-  export default recipes;
-  
-  
+  {
+    id: 1,
+    title: "Greek salad",
+    price: 12.99,
+    image: "https://media.istockphoto.com/id/1461256163/photo/feta-cheese-salad-on-wooden-table.jpg?s=612x612&w=is&k=20&c=UgdSQ54jsqpxbAE-6cAcqj47gXek4w8ubAkL6Uew8B8=",
+    description:
+      "The famous greek salad of crispy lettuce, peppers, olives and our Chicago style fota cheese, garnished with crunchy garlic and rosemary croutons",
+  },
+  {
+    id: 2,
+    title: "Bruchetta",
+    price: 5.99,
+    image: "https://cdn.pixabay.com/photo/2020/10/01/22/39/gourmet-5619887_1280.jpg",
+    description:
+      "Our Bruschetta is made from grilled bread that has been smeared with garlic and seasoned with salt and olive oil.",
+  },
+  {
+    id: 3,
+    title: "Lemon Dessert",
+    price: 4.78,
+    image: "https://www.eatingwell.com/thmb/bOOFDT9xXQSsyOokGa7THVTxn2o=/750x0/filters:no_upscale():max_bytes(150000):strip_icc()/8478639-a55e94717ea94cac990f8deded2399c0.jpg",
+    description:
+      "This comes straight from grandma's recipe book, every last ingredient has been sourced and is as authentic as can be imagined.",
+  },
+
+];
+
+export default recipes;
+
